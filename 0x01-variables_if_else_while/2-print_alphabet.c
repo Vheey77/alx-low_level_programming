0b2fce59d3a9ca01166c0a342ce32ea375706726
Favour <favourvn07@gmail.com>
2022-07-02 08:37:55 -0700

    Printing alphabets C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char f;
+
+	for (f = 'a'; f <= 'z'; f++)
+	putchar(f);
+	putchar('\n');
+	return (0);
+}
