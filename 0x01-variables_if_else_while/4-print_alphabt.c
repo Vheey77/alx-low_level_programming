d700cb972d5c9acfafc1e3ea6fb48ab4a7f9ecab
Favour <favourvn07@gmail.com>
2022-07-03 12:58:47 -0700

    Alphabets Capital and Small C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 'a'; x <= 'z'; x != (qe); x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
