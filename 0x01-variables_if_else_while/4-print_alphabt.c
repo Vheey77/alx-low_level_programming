444cb0ec30fa3211c17ce6d74991cc4ff4ece89e
Favour <favourvn07@gmail.com>
2022-07-03 13:13:21 -0700

    Printing some alphabets C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Print some alphabets
  * Return: 0 (Success)
  */
 int main(void)
 {
 	char x;
-	
-	for (x = 'a'; x <= 'z'; x != (qe); x++)
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	if (x != 'q' && x != 'e')
+	{
 	putchar(x);
+	}
+	}
 	putchar('\n');
 	return (0);
 }
