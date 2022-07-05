fd3bdd8520d39fc0c71d2f159b307aa899afac72
Favour <favourvn07@gmail.com>
2022-07-05 13:16:53 -0700

    Print alphabet x10 C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,18 @@
  *
  * Return: void
  */
-void print_alphabet_x10(void);
-{
-	int t;
+void print_alphabet_x10(void)
 
+{
+	int t, i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (t = 'a'; t <= 'z'; t++)
+		{
+			_putchar(t);
+		}
+		_putchar('\n');
+	}
+}
 
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 (0) A program that prints _putchar using the function _putchar
 (1) A program that prints the alphabet in lowercase
+(2) A program that prints the alphabet 10 times on different lines
